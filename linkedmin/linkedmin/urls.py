c27e5d1928f329af3dfdaffede4d8b1e456a6348
FEARmeR <fearmer@gmail.com>
2023-05-18 17:27:09 +0100

    Urls changed to pass not /admin and not /api

--- linkedmin/linkedmin/urls.py
+++ linkedmin/linkedmin/urls.py
@@ -13,12 +13,20 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+    from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('lm_api.urls')),
+]
 """
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('lm_api.urls')),
+    path('api/', include('lm_api.urls')),
 
 ]
