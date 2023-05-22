e77ef28c1612c956a145a0e8cb19d82b9579b1ab
kateschka <sova.krs@gmail.com>
2023-05-22 21:03:25 +0300

    changed gitignore

--- linkedmin/lm_api/serializers.py
+++ linkedmin/lm_api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from lm_projects.forms import ProjectForm
 from lm_users.forms import ProfileForm
 from lm_projects.models import Project, Tag, Review
-from lm_users.models import Profile, Message, Skill
+from lm_users.models import Profile, Message
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -93,7 +93,7 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     def patch(self, request, *args, **kwargs):
         form = ProfileForm(request.POST)
-        
+
         if request.FILES:
             form = ProfileForm(request.POST, request.FILES)
         if form.is_valid():
